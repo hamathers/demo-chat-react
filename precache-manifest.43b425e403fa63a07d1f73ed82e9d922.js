c459922edb62badda28ea3b764ca5c87c112e794
Apple <havubknp@gmail.com>
2021-11-26 13:53:46 +0700

    Updates

---
+++ precache-manifest.43b425e403fa63a07d1f73ed82e9d922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af43423697c941f28907b11eebb8c379",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab07376cdefb497b8607",
+    "url": "./static/css/main.1b8ac635.chunk.css"
+  },
+  {
+    "revision": "5986b1de894cdacaaea1",
+    "url": "./static/js/2.b2d958bb.chunk.js"
+  },
+  {
+    "revision": "45838b9b13cc516f68710720fccd81a3",
+    "url": "./static/js/2.b2d958bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab07376cdefb497b8607",
+    "url": "./static/js/main.22b84489.chunk.js"
+  },
+  {
+    "revision": "dfba7e6372e01ac0300c",
+    "url": "./static/js/runtime-main.2abb6eb7.js"
+  }
+]);
